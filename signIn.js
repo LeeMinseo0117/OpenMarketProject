03dffa062c712d131dc2ebfeab551b5d0b5d56f3
LeeMinseo0117 <leems3164@naver.com>
2024-08-05 17:36:24 +0900

    경고 문구 출력 작성

--- signIn.js
+++ signIn.js
@@ -1,3 +1,13 @@
+// **로그인 페이지**
+
+// - 아이디나 비밀번호가 일치하지 않거나, 아이디나 비밀번호를 입력하지 않은 채 로그인 버튼을 누르면 경고 문구가 나타납니다. -> 완성
+// - 입력 창 아래에 경고창이 나타나면 로그인 버튼을 눌러도 로그인 되지 않습니다.
+// - 입력 창에 입력이 안된 부분이 존재한 채로 로그인 버튼을 누르면 입력되지 않은 입력 창에 focus 이벤트가 작동하고 로그인은 되지 않습니다.
+// - 아이디나 비밀번호가 일치하지 않는다면, 비밀번호 입력창에 focus이벤트가 발생하고 빈칸이 됩니다.
+// - 로그인이 성공할 시, 로그인하기 이전 페이지로 이동합니다.
+// - 구매자 : 구매 회원 로그인 탭을 클릭하면 구매 회원으로 로그인합니다.
+// - 판매자 : 판매 회원 로그인 탭을 클릭하면 판매 회원으로 로그인합니다.
+
 const $form = document.querySelector("form");
 const $signIn = document.querySelector("#signIn");
 
@@ -45,13 +55,13 @@ $form.addEventListener("submit", async function (e) {
   const $pw = document.querySelector("#pw");
   const username = $id.value;
   const password = $pw.value;
-  const $alert = document.createElement("p");
+  const $alert = document.querySelector("#alert");
 
   // 2 아이디 입력창이 비어있는지 확인
   // 2-1 아이디 입력이 제대로 되어있지 않으면
   if (username.trim() === "") {
     // 2-2 YES~! -> 2-3 NO -> 3
-    alert("아이디를 입력해 주세요");
+    $alert.textContent = "아이디를 입력해 주세요.";
     // 2-3 경고 문구 출력 todo
     // 2-4 함수 종료
     return;
@@ -60,7 +70,7 @@ $form.addEventListener("submit", async function (e) {
   if (password.trim() === "") {
     // 3-1 비밀번호 입력이 제대로 되어있지 않으면
     // 3-2 YES~! -> 3-3 NO -> 4
-    alert("비밀번호를 입력해 주세요");
+    $alert.textContent = "비밀번호를 입력해 주세요";
     // 3-3 경고 문구 출력 todo
     // 3-4 함수 종료
     return;
@@ -82,14 +92,8 @@ $form.addEventListener("submit", async function (e) {
   }
   //   // 4-4 경고 문구 출력
   else {
-    alert("아이디 또는 비밀번호가 일치하지 않습니다");
+    $alert.textContent = "아이디 또는 비밀번호가 일치하지 않습니다.";
     // todo 문구 띄우기
   }
   // }
 });
-// A 아이디와 비밀번호 하나라도 입력 안하는지 확인
-// B 경고 문구 (아이디를 입력해 주세요) 비밀번호 입력창 밑에
-// C 경고 문구 (비밀번호를 입력해 주세요) 비밀번호 입력창 밑에
-// D 내가 입력한 아이디와 비밀번호가 서버 데이터와 일치하는지 확인
-// E 로그인
-// F 경고 문구 (아이디 또는 비밀번호가 일치하지 않습니다) 비밀번호 입력창 밑에
